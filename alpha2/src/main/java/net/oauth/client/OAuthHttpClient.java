8ee909efbf64444d647c9e17c30a7a2bf58ded18
jkristian@netflix.com <jkristian@netflix.com@f7ae4463-c52f-0410-a7dc-93bad6e629e8>
2007-11-14 18:09:38 +0000

    better diagnostics
    git-svn-id: http://oauth.googlecode.com/svn/code/java@246 f7ae4463-c52f-0410-a7dc-93bad6e629e8

--- alpha2/src/main/java/net/oauth/client/OAuthHttpClient.java
+++ alpha2/src/main/java/net/oauth/client/OAuthHttpClient.java
@@ -19,6 +19,7 @@ package net.oauth.client;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import net.oauth.OAuth;
 import net.oauth.OAuthMessage;
 import net.oauth.OAuthProblemException;
@@ -30,30 +31,45 @@ import org.apache.commons.httpclient.HttpStatus;
 public class OAuthHttpClient {
 
     /**
-         * Check whether a response indicates an OAuth problem. If so, throw an
+         * Check whether a response indicates an problem. If so, throw an
          * OAuthProblemException.
          */
     public static void checkResponse(HttpMethod method) throws IOException,
 	    OAuthProblemException {
 	int statusCode = method.getStatusCode();
 	if (statusCode != HttpStatus.SC_OK) {
-	    OAuthProblemException problem = new OAuthProblemException();
-	    problem.getParameters().put(OAuthProblemException.HTTP_STATUS_CODE,
+	    String statusText = method.getStatusText();
+	    Map<String, String> parameters = OAuth
+		    .newMap(getResponseParameters(method));
+	    String message = parameters
+		    .get(OAuthProblemException.OAUTH_PROBLEM);
+	    if (message == null) {
+		message = "HTTP status " + statusCode + " " + statusText;
+	    }
+	    OAuthProblemException problem = new OAuthProblemException(message);
+	    Map<String, Object> problemParameters = problem.getParameters();
+	    problemParameters.putAll(parameters);
+	    problemParameters.put("URL", method.getURI().toString());
+	    problemParameters.put(OAuthProblemException.HTTP_STATUS_CODE,
 		    new Integer(statusCode));
-	    problem.getParameters().put(OAuthProblemException.HTTP_STATUS_TEXT,
-		    method.getStatusText());
-	    problem.getParameters().put("URL", method.getURI().toString());
-	    problem.getParameters().putAll(
-		    OAuth.newMap(getResponseParameters(method)));
+	    problemParameters.put(OAuthProblemException.HTTP_STATUS_TEXT,
+		    statusText);
+	    if (statusCode == HttpStatus.SC_MOVED_TEMPORARILY
+		    || statusCode == HttpStatus.SC_MOVED_PERMANENTLY) {
+		Header location = method.getResponseHeader("Location");
+		if (location != null) {
+		    problemParameters.put("Location", location.getValue());
+		}
+	    }
 	    throw problem;
 	}
     }
 
     /**
          * Construct an OAuthMessage from the HTTP response, including
-         * parameters from OAuth WWW-Authenticate headers and the body if it's
-         * form encoded. The header parameters come first, followed by the ones
-         * from the response body.
+         * parameters from OAuth WWW-Authenticate headers and the body. The
+         * header parameters come first, followed by the ones from the response
+         * body.
          */
     public static OAuthMessage getResponseMessage(HttpMethod method)
 	    throws IOException {
